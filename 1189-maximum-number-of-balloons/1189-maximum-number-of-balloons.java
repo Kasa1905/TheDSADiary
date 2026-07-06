c5246853712d4a2f2d488d495feb86b6bee6cf95
Kaushik <sambekaushik@gmail.com>
2026-07-06 23:48:28 +0530

    Time: 2 ms (93.08%), Space: 43.1 MB (81.26%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        int[] f = new int[26];
+        for(char ch:text.toCharArray()){
+            f[ch-'a']++;
+        }
+        return Math.min(
+            Math.min(
+                Math.min(f['b'-'a'],f['a'-'a']),
+                Math.min(f['l'-'a']/2,f['o'-'a']/2)
+            ),
+            f['n'-'a']
+        );
+        
+    }
+}
